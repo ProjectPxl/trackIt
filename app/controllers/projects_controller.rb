2756c737d9f5d7403e3aa30509b9edcd80844a45
Paul Maltsev <maltsevp@gmail.com>
2016-07-07 20:58:17 -0700

    d

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,21 @@ class ProjectsController < ApplicationController
     @this_week  = @project.get_hours "week"
     @this_month = @project.get_hours "month"
     @all_hours = @project.hours.order(created_at: :desc)
+    # @invoice = @project.hours.where('created_at > ? AND created_at <= ?', "2016-02-01 06:37:17 -0800", "2016-03-01 20:25:48 -0800").order(created_at: :desc)
+    @invoice = @project.hours.where('created_at > ?', "2016-02-18 07:01:50 -0800").order(created_at: :desc)
+
+    @map = {}
+    @total = 0
+    @rate = 50
+    @invoice.each do |i|
+      issue = i.note.split(" ")[0]
+      if @map[issue]
+        @map[issue] += i.hours
+      else
+        @map[issue] = i.hours
+      end
+      @total += i.hours
+    end
   end
 
   def add_hours
